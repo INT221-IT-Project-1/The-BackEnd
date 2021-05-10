79ced6af3fdb55b43151a8b70dce9430a161e5fa
62130500093 <sahachai.kmutt@mail.kmutt.ac.th>
2021-05-11 02:23:41 +0700

    [add] image exception handling

---
+++ src/main/java/int221/backend/exceptions/ImageHandlerException.java
@@ -0,0 +1,17 @@
+package int221.backend.exceptions;
+
+import java.io.IOException;
+
+public class ImageHandlerException extends IOException {
+
+    ExceptionResponse.ERROR_CODE errorCode;
+    public ImageHandlerException(String message,ExceptionResponse.ERROR_CODE error_code){
+        super(message);
+        this.errorCode = error_code;
+    }
+
+    public ExceptionResponse.ERROR_CODE getErrorCode(){
+        return this.errorCode;
+    }
+}
+
